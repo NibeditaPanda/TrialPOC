3c6d1def7201780d218f0d15891648a22783a188
Maria <mgomez@thoughtworks.com>
2013-06-05 16:29:32 +0100

    #PROD-12|Loding csv files relative to root or all product price projects, to be able to run things from intellij

--- src/test/java/com.tesco.adapters/core/ControllerTest.java
+++ src/test/java/com.tesco.adapters/core/ControllerTest.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 import static com.tesco.adapters.core.PriceKeys.ITEM_NUMBER;
+import static com.tesco.adapters.core.PriceKeys.NATIONAL_PRICE;
 import static com.tesco.adapters.core.PriceKeys.ZONE_ID;
 import static org.fest.assertions.api.Assertions.assertThat;
 
@@ -21,7 +22,7 @@ public class ControllerTest {
         DBFactory.getCollection("prices").drop();
         collection = DBFactory.getCollection("prices");
 
-        String RPMPriceCsvFilePath = "src/test/java/com/tesco/adapters/rpm/fixtures/price.csv";
+        String RPMPriceCsvFilePath = "../PriceAdapters/src/test/java/com/tesco/adapters/rpm/fixtures/price.csv";
         new Controller(collection, RPMPriceCsvFilePath).fetchAndSaveBasePriceForProducts();
     }
 
@@ -30,7 +31,9 @@ public class ControllerTest {
     public void shouldImportPriceDataFromProductPriceRMSDump() {
         DBObject productWithPrice = collection.find((DBObject) JSON.parse(String.format("{\"%s\": \"050925811\"}", ITEM_NUMBER))).toArray().get(0);
 
-        assertThat(productWithPrice.get(ZONE_ID));
+        assertThat(productWithPrice.get(ZONE_ID)).isEqualTo("5");
+        assertThat(productWithPrice.get(ITEM_NUMBER)).isEqualTo("050925811");
+        assertThat(productWithPrice.get(NATIONAL_PRICE)).isEqualTo("1.33");
     }
 
 }
