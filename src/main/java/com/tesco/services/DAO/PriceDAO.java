6050672a6ac9d9dfa9e597eaa96d012761e02c80
minhngocd <mndang@thoughtworks.com>
2013-06-11 14:12:12 +0100

    #PROD-12|Chandan/Minno| pulled out constants

--- src/main/java/com/tesco/services/DAO/PriceDAO.java
+++ src/main/java/com/tesco/services/DAO/PriceDAO.java
@@ -12,6 +12,18 @@ import java.util.List;
 
 public class PriceDAO {
 
+    public static final String DEFAULT_CURRENCY = "GBP";
+    public static final String NATIONAL_ZONE = "5";
+
+    public static final String PRICES_COLLECTION = "prices";
+    public static final String STORES_COLLECTION = "stores";
+
+    public static final String ITEM_NUMBER = "itemNumber";
+    public static final String STORE_ID = "storeId";
+    public static final String ZONE_ID = "zoneId";
+    public static final String ZONES = "zones";
+    public static final String PRICE = "price";
+    public static final String CURRENCY = "currency";
 
     private final DBCollection priceCollection;
     private final DBCollection storeCollection;
@@ -19,44 +31,42 @@ public class PriceDAO {
 
     public PriceDAO(Configuration configuration) throws UnknownHostException {
         DBFactory dbFactory = new DBFactory(configuration);
-        priceCollection = dbFactory.getCollection("prices");
-        storeCollection = dbFactory.getCollection("stores");
+        priceCollection = dbFactory.getCollection(PRICES_COLLECTION);
+        storeCollection = dbFactory.getCollection(STORES_COLLECTION);
     }
 
     public DBObject getPriceByStore(String itemNumber, String storeId) {
-        List<DBObject> items = find(priceCollection, "itemNumber", itemNumber);
-        List<DBObject> stores = find(storeCollection, "storeId", storeId);
+        List<DBObject> items = find(priceCollection, ITEM_NUMBER, itemNumber);
+        List<DBObject> stores = find(storeCollection, STORE_ID, storeId);
         if (items.isEmpty() || stores.isEmpty()) return null;
 
         DBObject item = items.get(0);
         DBObject store = stores.get(0);
 
-        System.out.println("**** store info ****");
-        System.out.println(store);
-        System.out.println(item);
+        String zoneId = store.get(ZONE_ID).toString();
+        String currency = store.get(CURRENCY).toString();
 
-
-        String zone = store.get("zoneId").toString();
-        String currency = store.get("currency").toString();
-        String price = ((DBObject) ((DBObject) item.get("zones")).get(zone)).get("price").toString();
+        DBObject zones = (DBObject) item.get(ZONES);
+        DBObject zone = (DBObject) zones.get(zoneId);
+        String price = zone.get(PRICE).toString();
 
         return buildResponse(itemNumber, price, currency);
     }
 
     public DBObject getNationalPrice(String itemNumber) {
-        List<DBObject> items = find(priceCollection, "itemNumber", itemNumber);
+        List<DBObject> items = find(priceCollection, ITEM_NUMBER, itemNumber);
         if(items.isEmpty()) return null;
 
         DBObject item = items.get(0);
-        String price = ((DBObject) ((DBObject) item.get("zones")).get("5")).get("price").toString();
-        return buildResponse(itemNumber, price, "GBP");
+        String price = ((DBObject) ((DBObject) item.get(ZONES)).get(NATIONAL_ZONE)).get(PRICE).toString();
+        return buildResponse(itemNumber, price, DEFAULT_CURRENCY);
     }
 
     private DBObject buildResponse(String itemNumber, String price, String currency) {
         BasicDBObject responseObject = new BasicDBObject();
-        responseObject.put("itemNumber", itemNumber);
-        responseObject.put("price", price);
-        responseObject.put("currency", currency);
+        responseObject.put(ITEM_NUMBER, itemNumber);
+        responseObject.put(PRICE, price);
+        responseObject.put(CURRENCY, currency);
         return responseObject;
     }
 
