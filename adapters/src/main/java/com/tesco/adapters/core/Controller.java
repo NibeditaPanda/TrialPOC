133215d1fba856423154b43d0f430d9476255d76
Maria <mgomez@thoughtworks.com>
2013-06-27 18:04:07 +0100

    PROD56|Maria: importing data into a temp collection. When data is imported renamed the collection to the original one

--- adapters/src/main/java/com/tesco/adapters/core/Controller.java
+++ adapters/src/main/java/com/tesco/adapters/core/Controller.java
@@ -6,6 +6,8 @@ import com.tesco.adapters.rpm.RPMWriter;
 import org.slf4j.Logger;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.tesco.adapters.core.PriceKeys.*;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -30,21 +32,31 @@ public class Controller {
 
     public static void main(String[] args) {
         logger.info("Firing up...");
-        DBCollection priceCollection = DBFactory.getCollection(PRICE_COLLECTION);
-        DBCollection storeCollection = DBFactory.getCollection(STORE_COLLECTION);
 
         try {
             String RPMPriceZoneCsvFilePath = Configuration.get().getString("rpm.price.data.dump");
             String RPMStoreZoneCSVFilePath = Configuration.get().getString("rpm.store.data.dump");
             String RPMPromotionCsvFilePath = Configuration.get().getString("rpm.promotion.data.dump");
 
-            Controller controller = new Controller(priceCollection, storeCollection, RPMPriceZoneCsvFilePath, RPMStoreZoneCSVFilePath, RPMPromotionCsvFilePath);
+            DBCollection tempPriceCollection = DBFactory.getCollection(getTempCollectionName(PRICE_COLLECTION));
+            DBCollection tempStoreCollection = DBFactory.getCollection(getTempCollectionName(STORE_COLLECTION));
+
+            Controller controller = new Controller(tempPriceCollection, tempStoreCollection, RPMPriceZoneCsvFilePath, RPMStoreZoneCSVFilePath, RPMPromotionCsvFilePath);
             controller.fetchAndSavePriceDetails();
+
+            tempPriceCollection.rename(PRICE_COLLECTION, true);
+            tempStoreCollection.rename(STORE_COLLECTION, true);
+
         } catch (Exception exception) {
             logger.error("Error importing data", exception);
             throw new RuntimeException(exception);
         }
     }
+
+    private static String getTempCollectionName(String baseCollectionName) {
+        return String.format("%s%s", baseCollectionName, new SimpleDateFormat("yyyyMMdd").format(new Date()));
+    }
+
 
     public void fetchAndSavePriceDetails() throws IOException {
         indexMongo();
@@ -54,6 +66,5 @@ public class Controller {
     private void indexMongo() {
         priceCollection.ensureIndex(new BasicDBObject(ITEM_NUMBER, 1));
         storeCollection.ensureIndex(new BasicDBObject(STORE_ID, 1));
-
     }
 }
