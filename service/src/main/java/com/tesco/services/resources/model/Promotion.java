470fff96fc0053784bf0b3cdcbea347e3144f472
Alan Hampson <ahampson@thoguthworks.com>
2013-09-24 16:14:01 +0100

    prod-163 - addition of annotation to remove duplicate field when serializing to json

--- service/src/main/java/com/tesco/services/resources/model/Promotion.java
+++ service/src/main/java/com/tesco/services/resources/model/Promotion.java
@@ -1,7 +1,9 @@
 package com.tesco.services.resources.model;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
 public class Promotion {
 
     @JsonProperty(required = false)
