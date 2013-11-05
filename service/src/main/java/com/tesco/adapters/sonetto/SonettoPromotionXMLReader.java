987023ce673f9efbaff6b9222a54664948fc7697
Noppanit Charassinvichai <noppanit.c@gmail.com>
2013-11-05 13:32:30 +0000

    Toy - enable sonetty validation

--- service/src/main/java/com/tesco/adapters/sonetto/SonettoPromotionXMLReader.java
+++ service/src/main/java/com/tesco/adapters/sonetto/SonettoPromotionXMLReader.java
@@ -5,7 +5,6 @@ import com.tesco.adapters.sonetto.Elements.Promotion;
 import com.tesco.adapters.sonetto.Elements.Promotions;
 import org.xml.sax.SAXException;
 
-import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -15,6 +14,8 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.IOException;
+
+import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI;
 
 public class SonettoPromotionXMLReader {
     private SonettoPromotionWriter writer;
@@ -32,6 +33,12 @@ public class SonettoPromotionXMLReader {
 
         Unmarshaller u = JAXBContext.newInstance(Promotions.class).createUnmarshaller();
         Promotions promotions = u.unmarshal(source, Promotions.class).getValue();
+
+        SchemaFactory sf = SchemaFactory.newInstance(W3C_XML_SCHEMA_NS_URI);
+        Schema schema = sf.newSchema(new File(this.promotionsDataExportXsd));
+
+        Validator validator = schema.newValidator();
+        validator.validate(source);
 
         for (Promotion promotion : promotions.getStorePromotions()) {
             DBObject promotionDBObject = promotion.buildStoreDBObject(shelfURL);
