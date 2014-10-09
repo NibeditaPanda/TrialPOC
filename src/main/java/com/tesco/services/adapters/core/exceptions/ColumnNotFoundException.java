ae94ea4550853b59d6ff6d402a00e789d37c1a6c
unknown <QZ88@VDI01T11HSC-508.dotcom.tesco.org>
2014-10-09 10:53:19 +0100

    Modified to resolve sonarqube issues

--- src/main/java/com/tesco/services/adapters/core/exceptions/ColumnNotFoundException.java
+++ src/main/java/com/tesco/services/adapters/core/exceptions/ColumnNotFoundException.java
@@ -1,7 +1,14 @@
 package com.tesco.services.adapters.core.exceptions;
-
+//to handle exception if any column is not available in csv extract
 public class ColumnNotFoundException extends Exception {
+    /**
+     * <p>
+     *  exception to be thrown if a particular column is not available in csv file while import process
+     * </p>
+     * @param message
+     */
     public ColumnNotFoundException(String message) {
+
         super(message);
     }
 }
--- src/main/java/com/tesco/services/adapters/rpm/readers/PriceServiceCSVReaderImpl.java
+++ src/main/java/com/tesco/services/adapters/rpm/readers/PriceServiceCSVReaderImpl.java
@@ -15,14 +15,24 @@ import java.util.Map;
 
 import static com.tesco.services.adapters.core.utils.ExtractionUtils.getHeaderIndex;
 import static java.util.Arrays.asList;
-
+/**
+ * implements PriceServiceCSVReader interface, aims to read header information from csv files
+ */
 public class PriceServiceCSVReaderImpl implements PriceServiceCSVReader {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PriceServiceCSVReaderImpl.class);
 
     private final CSVReader csvReader;
     private Map<String, Integer> headerIndex = new HashMap<>();
-
+    /**
+     * <p>
+     * For reading the header information from csv files
+     * </p>
+     * @param filePath
+     * @param headers
+     * @throws IOException
+     * @throws ColumnNotFoundException
+     */
     public PriceServiceCSVReaderImpl(String filePath, String... headers) throws IOException, ColumnNotFoundException {
         this(new CSVReader(new InputStreamReader(new FileInputStream(filePath), "UTF-8"), ','),filePath, headers);
     }
